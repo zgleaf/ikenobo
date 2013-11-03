af46ad771bf6b36017d570a84ef55cd2249c8652
zhouguang <zgleaf@gmail.com>
2013-11-03 22:00:28 +0800

    init.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ikenobo::Application.config.secret_key_base = '28c66204e7223a53b7a261493bfc0a298aa37d40d7ed5910091ea2ab4aae908fdbf60278b77286705a3f167249e4d1b82c7e9bc94641f871df604fc28d63d34b'
